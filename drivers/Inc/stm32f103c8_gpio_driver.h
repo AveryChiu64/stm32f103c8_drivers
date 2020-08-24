a1df517ed8a0e23d1df185acbcc31eb11d0ed66a
Avery Chiu <averychiu@yahoo.com>
2020-08-24 16:45:30 -0400

    Added GPIO handle and configuration structures

--- drivers/Inc/stm32f103c8_gpio_driver.h
+++ drivers/Inc/stm32f103c8_gpio_driver.h
@@ -3,5 +3,20 @@
 
 #include "stm32f103c8.h"
 
+typedef struct
+{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+}GPIO_PinConfig_t;
+
+typedef struct
+{
+	// Pointer to hold base address of the GPIO Peripheral
+	GPIO_RegDef_t *pGPIOx;
+	GPIO_PinConfig_t GPIO_PinConfig;
+}GPIO_Handle_t;
 
 #endif /* INC_STM32F103C8_GPIO_DRIVER_H_ */
